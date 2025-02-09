72752dec666922577d1709cd537e4e96e14ee433
Mohamad Boutaam <167722785+maboutaam@users.noreply.github.com>
2025-02-08 20:48:47 -0500

    Update Instacart.py

--- Instacart.py
+++ Instacart.py
@@ -36,22 +36,6 @@ df_order_products = pd.read_csv("/datasets/order_products.csv", delimiter = ";")
 df_order_products.info()
 df_order_products.head()
 
-
-# <div class="alert alert-block alert-warning">📝
-#     
-# 
-# __Reviewer's comment №1__
-# 
-# 
-# I suggest separating logically different things:
-# 
-# 
-#     - data opening (this may not be a trivial process)
-#     - working code
-#     
-# This approach will improve code readability, reduce the time of searching for errors, if there are any, and exclude code re-execution
-# </div>
-
 # In[5]:
 
 
@@ -71,52 +55,11 @@ df_departments = pd.read_csv("/datasets/departments.csv", delimiter = ";")
 df_departments.info()
 df_departments.head()
 
-
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №1__
-# 
-# No familiarity with the data (use info() and head() methods
-
-# <div class="alert alert-block alert-warning">📝
-#     
-# 
-# __Reviewer's comment №1__
-# 
-# It would be good to comment on the data somehow
-
-# # 478966 orders, 49694 products, 4545007order products, 134 aisles, 21 departments.
-
-# ## Find and remove duplicate values (and describe why you make your choices)
-
-# ### `orders` data frame
-
 # In[7]:
 
 
 # working code
 df_instacart_orders.duplicated().sum()
-
-
-# <div class="alert alert-block alert-warning">📝
-#     
-# 
-# __Reviewer's comment №1__
-# 
-# 
-# I suggest separating logically different things:
-# 
-#     - library imports
-#     - data opening (this may not be a trivial process)
-#     - working code
-#     
-# This approach will improve code readability, reduce the time of searching for errors, if there are any, and exclude code re-execution
-# </div>
-
-# 
-
-# 
 
 # In[8]:
 
@@ -163,10 +106,6 @@ df_instacart_orders[df_instacart_orders.duplicated()]
 # working code
 df_instacart_orders[df_instacart_orders.duplicated(subset=['order_id'])]
 
-
-# <div class="alert alert-block alert-success">✔️
-#     
-# 
 # __Reviewer's comment №1__
 # 
 # Duplicate checking is the basis of data preprocessing
@@ -189,16 +128,6 @@ df_products.head()
 
 #working code
 df_products.duplicated().any()
-
-
-# 
-
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №1__
-# 
-# It's more efficient to load datasets once at the beginning of your project and then work with those loaded datasets throughout your analysis. Loading the same dataset repeatedly can be time-consuming and unnecessary. By loading the datasets only once at the start of the project, you can optimize your workflow and avoid redundant operations. Consider loading your datasets at the beginning and then utilizing them for all subsequent analysis to streamline your work and improve efficiency
 
 # In[15]:
 
@@ -335,39 +264,6 @@ df_order_products[df_order_products.duplicated(subset=['order_id', 'product_id']
 duplicates_check = df_order_products[df_order_products.duplicated(subset=['order_id', 'product_id'])]
 print(duplicates_check)
 
-
-# 
-
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №3__
-# 
-# We need to check for duplicate columns: 'order_id` and 'product_id'
-
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №4__
-# 
-# 
-# 
-# Can you please look at an example
-#     
-#     order_products[order_products.duplicated(subset=['order_id', 'product_id'])]
-
-# <div class="alert alert-block alert-success">✔️
-#     
-# 
-# __Reviewer's comment №5__
-# 
-# Now correct
-
-# ## Find and remove missing values
-# 
-
-# ### `products` data frame
-
 # In[31]:
 
 
@@ -416,23 +312,6 @@ df_departments.loc[df_departments['department_id'] == 21, 'department'].iloc[0]
 
 # working code
 df_products['product_name'] = df_products['product_name'].fillna('Unknown')
-
-
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №3__
-# 
-# No variable overwrite (df = df[].fillna())
-
-# <div class="alert alert-block alert-success">✔️
-#     
-# 
-# __Reviewer's comment №4__
-# 
-# You chose the most correct way to replace the omissions in the categorical column. Well done!)
-
-# ### `orders` data frame
 
 # In[37]:
 
@@ -519,26 +398,6 @@ df_order_products['add_to_cart_order'] = df_order_products['add_to_cart_order'].
 
 print(df_order_products.head())
 
-
-# <div class="alert alert-block alert-warning">📝
-#     
-# 
-# __Reviewer's comment №3__
-# 
-# 
-# Replacing it with 999/-1 etc is not really good. Tip for the future, look, it's not good to try to fill in the blanks left and right. In real work - replace with zero now, then forget and average count a month later. Plus there may be times when a bonus purchase or gift, will cost zero. In general, if it is possible to leave skips as skips - and here it can be done, it is better to do so, the results, even if we want to calculate something - it will not affect the results
-# 
-# It is better to use this code construction:
-#     
-#     data[''] = data[''].astype('Int64', errors='ignore')
-# </div>
-
-# 
-
-# # [A] Easy (must complete all to pass)
-
-# ### [A1] Verify that the `'order_hour_of_day'` and `'order_dow'` values in the `orders` tables are sensible (i.e. `'order_hour_of_day'` ranges from 0 to 23 and `'order_dow'` ranges from 0 to 6)
-
 # In[45]:
 
 
@@ -611,32 +470,6 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.show()
 
-
-# 
-
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №3__
-# 
-# There is a lack of visualization. Can you please add
-
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №4__
-# 
-# missing visualization. Can you please add
-
-# <div class="alert alert-block alert-success">✔️
-#     
-# 
-# __Reviewer's comment №5__
-# 
-# Well done
-
-# 
-
 # In[52]:
 
 
@@ -663,14 +496,6 @@ plt.show()
 
 
 # ## We can conclude from the above bar graph that the highest number of orders is at 10 AM. This was interpreted as a result of the highest number of orders 40578 was recorded at 10 am. This means that customers are most likely to go grocery shopping in the morning hours. 
-
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №3__
-# 
-# It is better after each section, graph (or series of tests) to write a conclusion on the obtained data taking into account the set business task - so it is easier to read the project, because future colleagues or customers will not have to interpret the results of each section, test or graph themselves.
-
 # In[54]:
 
 
@@ -890,17 +715,6 @@ plt.show()
 
 # ### The Top 20 products that customers are mostly organic produce, vegetables and fruits. This means that these products are bestsellers and popular among customers.
 
-# <div class="alert alert-block alert-danger">✍
-#     
-# 
-# __Reviewer's comment №3__
-# 
-# There is a lack of visualization. Can you please add
-
-# # [C] Hard (must complete at least two to pass)
-
-# ### [C1] How many items do people typically buy in one order? What does the distribution look like?
-
 # In[70]:
 
 
@@ -945,20 +759,6 @@ top_reordered_products = top_reordered_products.sort_values(by='reorder_count', 
 print(top_reordered_products.head(20)[['product_id', 'product_name']])
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# 
-
 # ### [C3] For each product, what proportion of its orders are reorders?
 
 # In[74]:
@@ -970,19 +770,6 @@ product_reorder_proportion = df_order_products.groupby('product_id')['reordered'
 product_reorder_proportion.columns = ['product_id', 'reorder_proportion']
 
 print(product_reorder_proportion)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
 
 # ### [C4] For each customer, what proportion of their products ordered are reorders?
 
@@ -1013,7 +800,3 @@ print(top_first_items_with_names[['product_id', 'product_name' , 'add_to_cart_or
 
 
 # ## After properly filtering all the data and visualizing the results. We can understand that customers are more likely to go grocery shopping on Saturday than any other day of the week. Also, they would prefer to go in the morning hours such as 10 AM. In addition, organic products and produce are highly popular among customers because they are the top 20 bestsellers. 
-
-# <div class="alert alert-block alert-success">✔️
-#     
-# 
